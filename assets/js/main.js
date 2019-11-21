3814108360c385dc4385a12eb93db0235137bffd
giuviglia94 <vigliaturo94@gmail.com>
2019-11-21 10:40:44 +0100

    pulizia codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,7 @@ var epifania;
           success : function (data) {
             var store = data.response;
             // console.table(store);
-            // console.log(store);
 
-            for (var i = 0; i < store.length; i++) {
-              capodanno = store[0].date;
-              epifania = store[1].date;
-
-              }
               var numeroDiGiorni = moment('01/01/2018', 'DD/MM/YYYY').daysInMonth();
               //Eseguo un ciclo per creare ciascun giorno del mese nel mio html
               for(var i = 1; i <= numeroDiGiorni; i++) {
@@ -27,30 +21,24 @@ var epifania;
                       // console.log(currentDay);
                       var currentMonth = moment(currentDate).format('MMMM');
 
-                      if (currentDate === epifania) {
-                        console.log("epifania");
-                        //Inserisco nell'html il mio div con attributo per eventuali selettori e la relativa data formattata
-                        $('.content').append('<div data-date="'+currentDate+'">'+currentMonth+" "+currentDay+"     "+"epifania"+'</div>');
 
+                      store.forEach(myFunction);
 
+                      function myFunction(item) {
+                        // console.log(item);
+                        if (currentDate === item.date) {
+                          console.log("festa");
+                          //Inserisco nell'html il mio li con attributo per eventuali selettori e la relativa data formattata
+                            $('.content').append('<li data-date="'+currentDate+'">'+currentMonth+" "+currentDay+" "+item.name+'</li>');
+
+                        }
+
+                        else {
+                            $('.content').append('<li data-date="'+currentDate+'">'+currentMonth+" "+currentDay+'</li>');
+                        }
                       }
-                      else if (currentDate === capodanno) {
-                        console.log("capodanno");
-
-                        //Inserisco nell'html il mio div con attributo per eventuali selettori e la relativa data formattata
-                        $('.content').append('<div data-date="'+currentDate+'">'+currentMonth+" "+currentDay+"    "+"capodanno"+'</div>');
-
-                      }
-                      else {
-                          $('.content').append('<div data-date="'+currentDate+'">'+currentMonth+" "+currentDay+'</div>');
-                      }
-
                   }
-
-
-
           },
-
 
           error : function (errore) {
                alert("E' avvenuto un errore. "+errore);
